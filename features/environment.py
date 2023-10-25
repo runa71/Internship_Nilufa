1d5025ce31c4799280d7206c518238ac67d8d29b
Nilufa <aminilufa71@gmail.com>
2023-10-25 14:56:14 -0400

    Task 03

--- features/environment.py
+++ features/environment.py
@@ -1,84 +1,3 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from webdriver_manager.chrome import ChromeDriverManager
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.support.wait import WebDriverWait
-#
-# from app.application import Application
-# from support.logger import logger
-#
-# # behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/bestsellers.feature
-#
-#
-# def browser_init(context, scenario_name):  # add scenario_name if you want to use it in Browserstack
-#     """
-#     :param context: Behave context
-#     """
-#     driver_path = ChromeDriverManager().install()
-#     service = Service(driver_path)
-#     context.driver = webdriver.Chrome(service=service)
-#     context.driver.maximize_window()
-#
-#     ### OTHER BROWSERS ###
-#     # service = Service(executable_path='/Users/svetlanalevinsohn/careerist/15-python-selenium-automation/geckodriver')
-#     # context.driver = webdriver.Firefox(service=service)
-#     # context.driver = webdriver.Safari()
-#
-#     ### HEADLESS MODE ####
-#     # options = webdriver.ChromeOptions()
-#     # options.add_argument('--headless')
-#     # service = Service(executable_path='/Users/svetlanalevinsohn/careerist/15-python-selenium-automation/chromedriver')
-#     # context.driver = webdriver.Chrome(
-#     #     options=options,
-#     #     service=service
-#     # )
-#
-#     ### BROWSERSTACK ###
-#     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
-#     # bs_user = ''
-#     # bs_key = ''
-#     # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-#     #
-#     # options = Options()
-#     # bstack_options = {
-#     #     'os': 'Windows',
-#     #     'osVersion': '10',
-#     #     'browserName': 'Firefox',
-#     #     'sessionName': scenario_name
-#     # }
-#     # options.set_capability('bstack:options', bstack_options)
-#     # context.driver = webdriver.Remote(command_executor=url, options=options)
-#
-#
-#     context.driver.maximize_window()
-#     context.driver.implicitly_wait(4)
-#     context.driver.wait = WebDriverWait(context.driver, 10)
-#
-#     context.app = Application(context.driver)
-#
-#
-# def before_scenario(context, scenario):
-#     print('\nStarted scenario: ', scenario.name)
-#     logger.info(f'\nStarted scenario: {scenario.name}')
-#     # Pass scenario.name to init() for browserstack config:
-#     browser_init(context, scenario.name)
-#
-#
-# def before_step(context, step):
-#     print('\nStarted step: ', step)
-#     logger.info(f'Started step: {step}')
-#
-#
-# def after_step(context, step):
-#     if step.status == 'failed':
-#         print('\nStep failed: ', step)
-#         logger.error(f'Step failed: {step}')
-#
-#
-# def after_scenario(context, feature):
-#     context.driver.delete_all_cookies()
-#     context.driver.quit()
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -93,10 +12,24 @@ def browser_init(context):
 
 # --- Chrome_browser test --- #
 
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
-    context.driver.maximize_window()
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+    # context.driver.maximize_window()
+
+###____Cross Browser Firefox____###
+    # context.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+
+###____ HEADLESS MODE ____ ####
+    options = webdriver.ChromeOptions()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--window-size=1000, 1000')
+    service = Service(r'/Users/nilufayesmin/Downloads/Internship/chromedriver.exe')
+    context.driver = webdriver.Chrome (options=options,  service=service)
+
+
 
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
